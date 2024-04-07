cec9e85baf8c461f3fd7b13d61af7ff671a7a5d7
surya kuruvadi <107850687+SuryaCodez@users.noreply.github.com>
2024-04-07 12:57:16 +0530

    Create ifelse.cpp

---
+++ ifelse.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(int a, int b) {
+	cout<<"enter the value of a:\n";
+	cin>>a;
+	cout<<"enter the value of b:\n";
+	cin>>b;
+	//a=5;
+	//b=3;
+	if(a>b){
+		cout<<"greater";
+	}
+	else if(a==b){
+		cout<<"equal";
+	}
+	else{
+		cout<<"smaller";
+	}
+	return 0;
+}
